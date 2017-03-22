537e4bb07c8072c0e3a6b241b79c9bbe378df26b
Chris Bird <chrisjbird@gmail.com>
2017-03-22 17:16:34 +0700

    tidy; ngOnInit()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
-
-// export class Hero {
-//   id: number;
-//   name: string;
-// }
-
-// const HEROES: Hero[] = [
-//   { id: 11, name: 'Mr. Nice' },
-//   { id: 12, name: 'Narco' },
-//   { id: 13, name: 'Bombasto' },
-//   { id: 14, name: 'Celeritas' },
-//   { id: 15, name: 'Magneta' },
-//   { id: 16, name: 'RubberMan' },
-//   { id: 17, name: 'Dynama' },
-//   { id: 18, name: 'Dr IQ' },
-//   { id: 19, name: 'Magma' },
-//   { id: 20, name: 'Tornado' }
-// ];
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -35,14 +18,6 @@ import { HeroService } from './hero.service';
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
-    // <div *ngIf="selectedHero">
-    //   <h2>{{selectedHero.name}} details!</h2>
-    //   <div><label>id: </label>{{selectedHero.id}}</div>
-    //   <div>
-    //     <label>name: </label>
-    //     <input [(ngModel)]="selectedHero.name" placeholder="name">
-    //   </div>
-    // </div>
     styles: [`
     .selected {
       background-color: #CFD8DC !important; 
@@ -93,16 +68,14 @@ import { HeroService } from './hero.service';
     }
   `],
   providers: [HeroService]
-  //template: `<h1>Hello {{name}}</h1>`,
 })
 
-export class AppComponent  { 
+export class AppComponent implements OnInit  { 
   constructor(private heroService: HeroService) {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   title = 'Tour of Heroes';
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
